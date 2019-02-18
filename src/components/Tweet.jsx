c0293008ad5865662b8265997dc6d3ad00b8a7ab
Paige Williams and Sheila Stephen <paige@>
2019-02-18 11:53:08 -0800

    Add styling to tweet list.

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -7,21 +7,17 @@ function Tweet (props){
     border: 'none',
     backgroundColor: 'white',
     margin: '10px 10px 0px 10px',
-    padding: '5px',
+    padding: '15px',
     fontFamily: 'Arial'
+
   };
 
-  const tweetContentStyle = {
-    margin: '-45px 5px 0px 60px',
-    paddingBottom: '3px'
-  }
+
 
   return (
     <div>
       <div style={tweetBoxStyles}>
-        <div style={tweetContentStyle}>
         <p>{props.tweet}</p>
-        </div>
       </div>
     </div>
   );
